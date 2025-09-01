548895571125c70dd2b22af7947ae6cfe5c54a4b
whu-pzhang <pzhang@whu.edu.cn>
2025-09-01 18:50:02 +0800

    update

--- dist_train.sh
+++ dist_train.sh
@@ -8,4 +8,4 @@ export NPROC_PER_NODE=2  # Adjust to the number of GPUs available
 
 export PYTHONPATH=$(pwd):$PYTHONPATH
 
-CUDA_VISIBLE_DEVICES=3,4 python train.py configs/spatiallm_sft_structured3d.yaml
+CUDA_VISIBLE_DEVICES=4,5 python train.py configs/spatiallm_sft_structured3d-pzhang.yaml
